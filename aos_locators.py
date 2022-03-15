04cbcfb2a5995fda7099cb077bf25bf2765d68cf
BaraaAlhamdani <baraa.alhamdani@outlook.com>
2022-03-15 01:44:38 -0700

    Lab 6 updated

--- aos_locators.py
+++ aos_locators.py
@@ -1,5 +1,6 @@
 from faker import Faker
 fake = Faker(locale='en_CA')
+import random
 # -------------- Locators section ---------------
 AOS_url = 'https://advantageonlineshopping.com/#/'
 AOS_title = '\xa0Advantage Shopping'
@@ -9,9 +10,10 @@ AOS_chat_with_us_url = 'https://advantageonlineshopping.com/chat.html'
 AOS_chat_with_us_title = 'Advantage Online Shopping Demo Support Chat'
 AOS_order_payment = 'https://advantageonlineshopping.com/#/orderPayment'
 
+
 # -------- data section -------------
-first_name = fake.first_name()
-last_name = fake.last_name()
+first_name = fake.first_name()[:7]
+last_name = fake.last_name()[:7]
 middle_name = fake.first_name()
 full_name = f'{first_name}{""} {last_name}'
 new_username = f'{first_name}{last_name}'.lower()
@@ -25,5 +27,7 @@ address = fake.street_address()
 province = fake.province()[:10]
 postal_code = fake.postcode()
 description = fake.sentence(nb_words=20)
+random_product = random.randint(1, 34)
+AOS_items = f'https://advantageonlineshopping.com/#/product/{random_product}'
 
 #-------------------------------------------------------------
